685f7569b1c4450f7bad092c30fd73f2f16111bb
SakataHirofumi <sumlibe@gmail.com>
2016-08-24 08:46:10 +0900

    traceパッケージの復活

--- room.go
+++ room.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	"github.com/sumlibe/gochat/trace"
 )
 
 type room struct {
@@ -17,7 +18,7 @@ type room struct {
 	// clientsには在室しているすべてのクライアントが保持されます
 	clients map[*client]bool
 	// tracerはチャットルーム上で行なわれた操作のログを受け取ります
-	// tracer trace.Tracer
+	tracer trace.Tracer
 }
 
 // newRoomはすぐに利用できるチャットルームを生成して返す
@@ -27,7 +28,7 @@ func newRoom() *room {
 		join:    make(chan *client),
 		leave:   make(chan *client),
 		clients: make(map[*client]bool),
-		// tracer:  trace.Off(),
+		tracer:  trace.Off(),
 	}
 }
 
@@ -37,25 +38,25 @@ func (r *room) run() {
 		case client := <-r.join:
 			// 参加
 			r.clients[client] = true
-			// r.tracer.Trace("新しいクライアントが参加しました")
+			r.tracer.Trace("新しいクライアントが参加しました")
 		case client := <-r.leave:
 			// 退室
 			delete(r.clients, client)
 			close(client.send)
-			// r.tracer.Trace("クライアントが退室しました")
+			r.tracer.Trace("クライアントが退室しました")
 		case msg := <-r.forward:
-			// r.tracer.Trace("メッセージを受信しました: ", string(msg))
+			r.tracer.Trace("メッセージを受信しました: ", string(msg))
 			// すべてのクライアントにメッセージを転送
 			for client := range r.clients {
 				select {
 				case client.send <- msg:
 					// メッセージを送信
-					// r.tracer.Trace(" -- クライアントに送信されました")
+					r.tracer.Trace(" -- クライアントに送信されました")
 				default:
 					// 送信に失敗
 					delete(r.clients, client)
 					close(client.send)
-					// r.tracer.Trace(" -- 送信に失敗しました。クライアントをクリーンアップします")
+					r.tracer.Trace(" -- 送信に失敗しました。クライアントをクリーンアップします")
 				}
 			}
 		}
